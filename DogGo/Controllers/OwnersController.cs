02a3aba6e6396ebe6e1dc1e96eedd95f6c760f28
Holden Parker <fuquardwork@gmail.com>
2020-06-19 11:40:32 -0500

    refactored owner/edit view

--- DogGo/Controllers/OwnersController.cs
+++ DogGo/Controllers/OwnersController.cs
@@ -94,30 +94,33 @@ namespace DogGo.Controllers
         // GET: Owners/Edit/5
         public ActionResult Edit(int id)
         {
+            List<Neighborhood> neighborhoods = _neighborhoodRepo.GetAll();
             Owner owner = _ownerRepo.GetOwnerById(id);
 
-            if (owner == null)
+            OwnerFormViewModel vm = new OwnerFormViewModel()
             {
-                return NotFound();
-            }
+                Owner = owner,
+                Neighborhoods = neighborhoods
+            };
 
-            return View(owner);
+            return View(vm);
         }
+        
 
         // POST: Owners/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, Owner owner)
+        public ActionResult Edit(int id, OwnerFormViewModel vm)
         {
             try
             {
-                _ownerRepo.UpdateOwner(owner);
+                _ownerRepo.UpdateOwner(vm.Owner);
 
                 return RedirectToAction("Index");
-            }
+            }                           
             catch (Exception ex)
             {
-                return View(owner);
+                return View(vm);
             }
         }
 
